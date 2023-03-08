b7265a79afd8b9122c2b7cf81e60da43cbe89218
pritumdebnath <122291993+pritumdebnath@users.noreply.github.com>
2023-03-08 16:56:28 -0500

    new push

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,21 @@
 // controller actions
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 //handle errors
 const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = {email: '', password: ''};
+
+    //incorrect email
+    if (err.message === 'incorrect email') {
+        errors.email = 'That email is not registered';
+    }
+
+    //incorrect password
+    if (err.message === 'incorrect password') {
+        errors.password = 'That password is incorrect';
+    }
 
     //duplicate errors
     if (err.code === 11000){
@@ -21,6 +32,14 @@ const handleErrors = (err) => {
     return errors;
 }
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+    //returns the token
+    return jwt.sign({ id }, 'secret message', {
+        expiresIn: maxAge
+    });
+}
+
 
 module.exports.signup_get = (req, res) => {
     res.render('signup');
@@ -34,7 +53,9 @@ module.exports.signup_post = async (req, res) => {
     const {email, password} = req.body;
     try{
         const user = await User.create({ email, password });
-        res.status(201).json(user);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000});
+        res.status(201).json({ user: user._id });
     }
     catch(err){
         const errors = handleErrors(err);
@@ -45,6 +66,13 @@ module.exports.signup_post = async (req, res) => {
 module.exports.login_post = async (req, res) => {
     const {email, password} = req.body;
     
-    console.log(email, password)
-    res.send('user login');
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000});
+        res.status(200).json({ user: user._id });
+    } catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
+    }
 }
