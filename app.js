76e1c755617d0ecb1ed27461d42fc4f648a3430c
pritumdebnath <122291993+pritumdebnath@users.noreply.github.com>
2023-03-15 16:58:11 -0400

    new push

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -18,7 +21,32 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
   .catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 
 app.use(authRoutes);
+
+//cookies
+
+//create a cookie
+// app.get('/set-cookies', (req, res) => {
+
+  // res.setHeader('Set-cookie', 'newUser=true');
+
+  //does the same as above, looks cleaner
+  // res.cookie('newUser', false);
+  // res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+  // res.send('You got the cookie, check the Application tab in the inspect tool!');
+
+// });
+
+//read a cookie
+// app.get('/read-cookies', (req, res) => {
+
+//   const cookies = req.cookies;
+//   console.log(cookies.newUser);
+
+//   res.json(cookies);
+
+// });
